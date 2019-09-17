d7b4bb6588992bea45e90a721f2b5077aaff99d9
nikolajwassmann <nwassmann@hotmail.com>
2019-09-17 09:32:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hovedmenu/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.hovedmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button knap1;
+    Button knap2;
+    Button søg;
+    ImageView billede;
+    TextView overskrift;
+    TextView tekst;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        knap1 = findViewById(R.id.indstillinger);
+        knap2 = findViewById(R.id.vejledning);
+        billede = findViewById(R.id.billede);
+        overskrift = findViewById(R.id.overskrift);
+        tekst = findViewById(R.id.tekst);
+        billede.setImageResource(R.drawable.icon);
+        søg = findViewById(R.id.søg);
+
+        knap1.setOnClickListener(this);
+        knap2.setOnClickListener(this);
+        søg.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == knap1){
+            tekst.setText("Tekst 1");
+
+
+        }
+        if (view == knap2){
+            tekst.setText("Tekst 2");
+        }
+
+        if (view == søg){
+            webView = new WebView(this);
+            webView.loadUrl("https://javabog.dk");
+            setContentView(webView);
+        }
+
+    }
+}
